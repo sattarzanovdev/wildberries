6b5bc66da4806bfed09152f1f3f77a060cffa925
Sattarzanov Daniel <sattarzanovdev@gmail.com>
2022-04-29 17:00:32 +0600

    finish

--- js/script.js
+++ js/script.js
@@ -1,123 +1,160 @@
 const $container = document.querySelector('.container')
 const $search = document.querySelector('.search')
 const $modal = document.querySelector('.modal-window')
-const $close = document.querySelector('.fa-times')
 const $send = document.querySelector('.send')
 const $modalSuccess = document.querySelector('.modal_success')
-const $closeModal = document.querySelector('.close_modal')
 const $address = document.querySelector('.address')
 const $telephone = document.querySelector('.telephone')
 const $emailModal = document.querySelector('.emailModal')
 const $textModal = document.querySelector('.textModal')
+const $titleGood = document.querySelector('.titleGood')
+const $descriptionGood = document.querySelector('.descriptionGood')
+const $priceGood = document.querySelector('.priceGood')
+const $imageGood = document.querySelector('.imageGood')
+const $categoryGood = document.querySelector('.categoryGood')
+const $createGoodBtn = document.querySelector('.createGood')
 
 const BASE_URL = 'https://pbasics.pythonanywhere.com'
 
 window.addEventListener('load', () => {
-  fetch(`${BASE_URL}/products`)    
-    .then(res => res.json())    
-    .then(res => cardTemplate(res))
+  getRequest('products')
 })
-
-function getRequest(cb){
-  fetch(`${BASE_URL}/products`)    
-  .then(r => r.json())    
-  .then(res => cb(res))
-}
 
 $search.addEventListener('input', e => {
   let value = e.target.value.toUpperCase()
-    getRequest(cb => {
+    getRequest('products', cb => {
       const filtered = cb.filter(item => item.title.toUpperCase().includes(value))
       cardTemplate(filtered)
     })
 })
 
+function getRequest(endPoint){
+  fetch(`${BASE_URL}/${endPoint}`)    
+  .then(r => r.json())    
+  .then(res => cardTemplate(res))
+}
+
 function cardTemplate(base){
-  const template = base.map(({id, title, price, image, description}) => {
+  console.log(base);
+  const template = base.map(item => {
     return `
       <div class="card">
         <div class="card-image">
-          <img src="${image}">
+          <img src="${item.image ? item.image : item.image_url}">
         </div>
         <div class="card-body">
-          <div class="about"><h2>${title}:</h2><p>${description}</p></div>
-          <p class="notThisPrice">${price + 100}руб</p>
-          <p>${price}руб</p>
+          <div class="about"><h2>${item.title}:</h2><p>${item.description}</p></div>
+          <p class="notThisPrice">${item.price + 100}руб</p>
+          <p>${item.price}руб</p>
         </div>
         <div class="card-footer">
-          <button onclick="Buy('${id}')">Купить</button>
+          <button onclick="Edit('${item.id}')">Изменить</button>
+          <button onclick="deleteProducts('${item.id}')">Удалить</button>
         </div>
       </div>
-    `
-  }).join('')
-
-  $container.innerHTML = template
+      `
+  })
+    $container.innerHTML = template.reverse()
 }
 
 
-function Buy(id){
-  fetch(`${BASE_URL}/products`)
+function getProducts(){
+  fetch(`${BASE_URL}/products`,{
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
+  .then(r => r.json())
+  .then(res => {
+    cardTemplate(res)
+  })
+}
+
+function createProducts(){
+  fetch(`${BASE_URL}/products/create/ `, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: $titleGood.value,
+      description: $descriptionGood.value,
+      price: $priceGood.value,
+      image_url: $imageGood.value,
+      category: +$categoryGood.value,
+    })
+  })
+  .then(res => res.json())
+  .then(cardTemplate)
+
+}
+
+$createGoodBtn.addEventListener('click', e => {
+  e.preventDefault()
+
+  createProducts()
+})
+
+function deleteProducts(id){
+  fetch(`${BASE_URL}/products/delete/${id}`, {
+    method:'DELETE',
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
+  .then(cardTemplate)
+}
+
+const $addGood = document.querySelector('.add_good')
+const $addGoodBtn = document.querySelector('.add')
+const $closeAddGood = document.querySelector('.fa-times')
+
+$addGoodBtn.addEventListener('click', e => {
+  e.preventDefault()
+
+  $addGood.classList.toggle('active')
+})
+
+$closeAddGood.addEventListener('click', e => {
+  e.preventDefault()
+
+  $addGood.classList.remove('active')
+})
+
+function Edit(id){
+  fetch(`${BASE_URL}/products/update/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-type':'application/json'
+    },
+    body: JSON.stringify({
+      title: prompt('Название товара'),
+      description: prompt('Описание товара'),
+      price: prompt('Цена товара'),
+      category: prompt('Категория товара'),
+      image: prompt('Фото товара'),
+    })
+  })
     .then(res => res.json())
-    .then(res => buyTemplate(res[id - 1]))
+    .then(getRequest)
 }
 
-function buyTemplate(base){
-  const buyTemplate = `
-    <button class="back" onclick="back()" ><i class="fa fa-arrow-left"></i></button>
-    <div class="moreCard">
-      <div class="more-image">
-        <img src="${base.image}">
-      </div>
-      <div style="display: flex; flex-direction: column">
-        <div class="more-info">
-          <div style="display: flex; align-items: center">
-            <h1>${base.title}: </h1> <p style="margin-left: 5px">${base.description}</p>
-          </div>
-          <p class="notThisPrice">${base.price + 100} руб</p>
-          <p>${base.price} руб</p>
-        </div>
-        <div class="more-footer">
-          <button onclick="modal()">Заказать одним кликом</button>
-        </div>
-      </div>
-    </div>
-  `
-
-  $container.innerHTML = buyTemplate
-}
-
-function modal(){
-  $modal.classList.add('active')
-  document.body.setAttribute('style', 'background: rgba(0, 0, 0, 0.5)')
-}
-
-$close.addEventListener('click', () => {
-  $modal.classList.remove('active')
-  document.body.removeAttribute('style')
-})
-
-$send.addEventListener('click', e => {
-  e.preventDefault()
-
-  if($address.value.length === 0 || $telephone.value.length === 0 || $emailModal.value.length === 0){
-    $textModal.innerHTML = 'Заполните!'
-    $modalSuccess.classList.add('active')
-    $modal.classList.remove('active')
-    document.body.removeAttribute('style')
-  }else{
-    $textModal.innerHTML = 'Успешно!'
-    $modalSuccess.classList.add('active')
-    $modal.classList.remove('active')
-    document.body.removeAttribute('style')
-  }
-})
-
-$closeModal.addEventListener('click', e => {
-  e.preventDefault()
-
-  $modalSuccess.classList.remove('active')
-})
-
-function back(){
-  window.location.reload()
+function Edit(id){
+  console.log(id);
+  fetch(`${BASE_URL}/products/update/${id}`,{
+    method:'PATCH',
+    headers:{
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: prompt('Название товара'),
+      description: prompt('Описание товара'),
+      price: +prompt('Цена товара'),
+      category: +prompt('Категория товара'),
+      image_url: prompt('Фото товара'),
+    })
+  })
+  .then(res => res.json())
+  .then(getProducts)
 }
